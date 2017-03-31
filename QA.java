2b2ad291fe1b0591fb83dbcd5127847128a80c85
Milo <mikkelmilo@gmail.com>
2017-03-31 10:53:10 +0200

    added QA

---
+++ QA.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class QA implements Serializable {
+    private String question;
+    private String answer;
+
+    public void setQuestion(String q) {
+	question = q;
+    }
+
+    public String getQuestion() {
+	return question;
+    }
+
+    public void setAnswer(String q) {
+	question = q;
+    }
+
+    public String getAnswer() {
+	return question;
+    }
+}
