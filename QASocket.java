a852091aa7038895345c7d65aa5afb69c722b9ae
Milo <mikkelmilo@gmail.com>
2017-04-04 19:47:32 +0200

    added qasocket

--- QASocket.java
+++ QASocket.java
@@ -1,12 +1,17 @@
 import java.net.Socket;
-
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 // wrapper
 public class QASocket{
 	public Socket socket;
 	public QA qa;
+	public ObjectInputStream inputStream;
+	public ObjectOutputStream outputStream;
 
-	public QASocket(QA qa, Socket socket){
+	public QASocket(QA qa, Socket socket, ObjectOutputStream out, ObjectInputStream in){
 		this.socket = socket;
 		this.qa = qa;
+		inputStream = in;
+		outputStream = out;
 	}
 }
