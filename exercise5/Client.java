f88c0b69800422e10b1b9ebc8f802d91c2c67a18
Sophie Mai Chau <sophie_chau@hotmail.com>
2017-04-11 14:39:19 +0200

    editor works on both computers and vm, but not as client in vm

--- exercise5/Client.java
+++ exercise5/Client.java
@@ -5,59 +5,62 @@ import java.io.IOException;
 
 public class Client implements Runnable {
 
-  ConnectionHandler handler;
-  DocumentEventCapturer doc;
-  String serverName;
-  private EventReplayer er;
-  private Socket socket = null;
-  int port = 40499;
+	ConnectionHandler handler;
+	DocumentEventCapturer doc;
+	String serverName;
+	private EventReplayer er;
+	private Socket socket = null;
+	int port = 40499;
 
-  public Client(String serverName, EventReplayer er, int port) {
-    this.er = er;
-    this.doc = doc;
-    this.port = port;
-    this.serverName = serverName;
-  }
+	public Client(String serverName, EventReplayer er, int port) {
+		this.er = er;
+		this.doc = doc;
+		this.port = port;
+		this.serverName = serverName;
+	}
 
-  public void run() {
-    System.out.println("Starting client. Type CTRL-D to shut down.");
-    //printLocalHostAddress();
-    socket = connectToServer(serverName);
-    if (socket != null) {
-      System.out.println("Connected to " + socket);
-      try {
-        //For sending objects to the server
-        ObjectOutputStream objOutStream = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream objInputStream = new ObjectInputStream(socket.getInputStream());
-        objOutStream.flush();
-        handler = new ConnectionHandler(socket, objInputStream, objOutStream);
-        er.setConnectionHandler(handler);
-      } catch (IOException e) {
-        System.err.println(e);
-        handler.closeConnection();
-      }
-    }
-  }
-  
-  public void disconnect() {
-    handler.closeConnection();
-  }
+	public void run() {
+		System.out.println("Starting client. Type CTRL-D to shut down.");
+		// printLocalHostAddress();
+		socket = connectToServer(serverName);
+		if (socket != null) {
+			System.out.println("Connected to " + socket);
+			try {
+				// For sending objects to the server
+				ObjectOutputStream objOutStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream objInputStream = new ObjectInputStream(socket.getInputStream());
+				objOutStream.flush();
+				handler = new ConnectionHandler(socket, objInputStream, objOutStream);
+				er.setConnectionHandler(handler);
+			} catch (IOException e) {
+				System.err.println(e);
+				if (handler != null) {
+					handler.closeConnection();
+				}
+			}
+		}
+	}
 
-  public ConnectionHandler getConnectionHandler() {
-    return handler;
-  }
+	public void disconnect() {
+		handler.closeConnection();
+	}
 
-  /**
-  *
-  * Connects to the server on IP address serverName and port number portNumber.
-  */
-  protected Socket connectToServer(String serverName) {
-    Socket res = null;
-    try {
-      res = new Socket(serverName, port);
-    } catch (IOException e) {
-      // We return null on IOExceptions
-    }
-    return res;
-  }
+	public ConnectionHandler getConnectionHandler() {
+		return handler;
+	}
+
+	/**
+	 *
+	 * Connects to the server on IP address serverName and port number
+	 * portNumber.
+	 */
+	protected Socket connectToServer(String serverName) {
+		Socket res = null;
+		try {
+			res = new Socket(serverName, port);
+		} catch (IOException e) {
+			// We return null on IOExceptions
+		}
+		return res;
+	}
 }
